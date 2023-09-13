dd3373322de8415813f5aaa911988ddaa246c2f4
PilyaP <ppaexpert@gmail.com>
2023-09-13 16:20:57 +0300

    заболел

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { ModalContainer, Overlay } from './Modal.styled';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ showModal, onClose, children }) => {
+  useEffect(() => {
+    if (showModal) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.body.style.overflow = 'auto';
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose, showModal]);
+
+  const handleOverlayClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <ModalContainer>{children}</ModalContainer>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+export default Modal;S
